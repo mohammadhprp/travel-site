c6de41196c8af487066c49485e2372176e0fb424
Mohammad <18.mamali@gmail.com>
2021-08-24 17:21:34 +0430

    Almost completed build-process [js,css and html]

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const postCSSPlugins = [
   require('postcss-import'),
@@ -9,13 +13,28 @@ const postCSSPlugins = [
   require('autoprefixer')
 ]
 
-module.exports = {
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
+  plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+}
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
-  },
-  devServer: {
+  }
+  config.devServer = {
     before: function(app, server) {
       server._watch('./app/**/*.html')
     },
@@ -23,14 +42,23 @@ module.exports = {
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-      }
-    ]
   }
+  config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSPlugins.push(require('cssnano'))
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: {chunks: 'all'}
+  }
+  config.plugins.push(new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}))
+}
+
+module.exports = config
