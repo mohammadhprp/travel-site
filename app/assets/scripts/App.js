7ada329c29b12b7a9ff75f321ed1c95cd5a01162
Mohammad <18.mamali@gmail.com>
2021-08-28 16:39:31 +0430

    add react to the site

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,24 @@
 import '../styles/styles.css'
 import 'lazysizes'
-
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 import ClientArea from './modules/ClientArea'
 
+// React Related Code Goes Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import React components that we created
+import MyAmazingComponent from './modules/MyAmazingComponent'
+
+// ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"))
+
+new ClientArea()
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
-new MobileMenu()
-new ClientArea()
+new MobileMenu();
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
